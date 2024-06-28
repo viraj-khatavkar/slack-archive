7ac8ae68b5892eb7eef1c3dcf22348f7af4e2dac
Viraj Khatavkar <viraj.2438@gmail.com>
2024-06-28 14:17:23 +0530

    wip

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,11 @@
 
         <title>{{ config('app.name') }}</title>
 
+        <link rel="apple-touch-icon" sizes="180x180" href="/images/logo.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/images/logo.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/images/logo.png">
+
+
         <!-- Fonts -->
 
         <!-- Styles -->
